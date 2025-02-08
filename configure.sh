3ba5b3d0aba304824fda7028415210d0b86b217d
黑歌 <47450409+dysf888@users.noreply.github.com>
2025-02-09 01:01:30 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,4 +6,6 @@ sed -i 's/192.168.1.1/192.168.100.252/g' package/base-files/files/bin/config_gen
 
 # 5G
 echo "src-git 5g https://github.com/Siriling/5G-Modem-Support;main" >> feeds.conf.default
+#Nezha
+echo "src-git nezha https://github.com/dysf888/OpenWRT_NeZha_V1" >> feeds.conf.default
 
